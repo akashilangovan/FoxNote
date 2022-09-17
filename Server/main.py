0bf06a6d36e7cef11505be81d42200b4389ecf5f
Alamgir Khan <k.alamgir2002@gmail.com>
2022-09-17 12:47:27 -0400

    implemented summarize transcript endpoint with cohere api

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/summarize/")
+def summarize(transcript: str):
+    import cohere
+    co = cohere.Client('LXa6tCc1qEGSshUxwewWPOJo5HcJ81tW5rkd01Jr')
+
+    response = co.generate(
+    model='large',
+    prompt=transcript,
+    return_likelihoods = 'GENERATION',
+    stop_sequences=['"'],
+    max_tokens=50,
+    temperature=0.2,
+    num_generations=1,
+    k=0,
+    p=0.75)
+
+    return {response.generations[0].text}
+
+
