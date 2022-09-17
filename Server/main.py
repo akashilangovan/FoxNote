2e52fff4e304de8fafd3821490b9ea78c55f14b0
richardld <32209816+richardld@users.noreply.github.com>
2022-09-17 17:12:44 -0400

    Change server

--- Server/main.py
+++ Server/main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import requests
+import cohere
+
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 PROMPT = """
-Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn’t the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to “the dusty section of the dictionary” to find its latest words.
+Passage: Welcome to CS 61A! Lecture will begin at 2:10pm. Until then, you just get to watch us type. Looking for something to do? Try reading the "Syllabus" link at the top of cs61a.org. Feel free to ask questions in the Zoom chat.
 
-TLDR: Wordle has not gotten more difficult to solve.
+TLDR: We will be starting lecture soon.
 --
-Passage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.
+Passage:  Midterm One is Monday and homework and the hog party are meant to help you get ready for Midterm One. We've posted out description of the logistics. You should read that. You will be provided with a printed copy of the Midterm One Study guide. Of Sliders from the lectures. So that if you ever want to look up evaluation rule or execution rule for particular kind of expression or statement, you can do that.
 
-TLDR: ArtificialIvan has raised $190 million in Series C funding.
+TLDR: Midterm One is coming up. Read the logistics. You will have a study guide during the exam.
 --
 Passage: The National Weather Service announced Tuesday that a freeze warning is in effect for the Bay Area, with freezing temperatures expected in these areas overnight. Temperatures could fall into the mid-20s to low 30s in some areas. In anticipation of the hard freeze, the weather service warns people to take action now.
 
@@ -18,29 +34,38 @@ TLDR: A hard freeze is expected in the Bay Area overnight.
 Passage:
 """
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/summarize/")
+@app.get("/summarize/")
 def summarize(transcript: str):
-    import cohere
+    print("SDFDSG", str)
     co = cohere.Client('LXa6tCc1qEGSshUxwewWPOJo5HcJ81tW5rkd01Jr')
 
-    engineered_prompt = PROMPT + transcript + "\n\nTLDR:"
+    engineered_prompt = PROMPT + transcript + "\n--\nTLDR: "
     
     response = co.generate(
-    model='large',
-    prompt=engineered_prompt,
-    stop_sequences=['--'],
-    max_tokens=50,
-    temperature=0.65,
-    num_generations=1,
+        model='large',
+        prompt=engineered_prompt,
+        stop_sequences=['--'],
+        max_tokens=50,
+        temperature=0.92,
+        num_generations=1,
     )
-@app.post("/token/")
+    
+    text = response.generations[0].text
+    if "\n" in text:
+        text = text[:text.index("\n")]
+    print(text)
+    return {"text": text}
+    
+@app.get("/token/")
 def getToken():
-    PARAMS = {'expires_in':3600}
+    PARAMS = {'expires_in':36000}
     HEADERS = {'authorization':'1298b52db56e479f8363db36d5c0e8dc'} 
     response = requests.post('https://api.assemblyai.com/v2/realtime/token', headers = HEADERS, json=PARAMS)
+    print(response.json())
     return response.json()
 
